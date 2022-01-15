5470fdb065e882d1e72e0d1c377ced6b29fc59f8
xtang727 <xtang727@outlook.com>
2022-01-14 22:50:06 -0500

    Inital Commit

---
+++ dags/spotify_dag.py
@@ -0,0 +1,32 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+from spotify_etl import run_spotify_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 1, 15),
+    'email': ['someone@anemail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'spotify_dag',
+    default_args=default_args,
+    description='Spotify ETL DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+run_etl = PythonOperator(
+    task_id='spotify_etl',
+    python_callable=run_spotify_etl,
+    dag=dag,
+)
+
+run_etl
